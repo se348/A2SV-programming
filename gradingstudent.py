7686bdacc4969ea255ecd97dd6c64149ae5e5be0
se348 <semir2578@gmail.com>
2022-03-25 00:11:31 -0700

    solved grading student hackerrank

---
+++ gradingstudent.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    end=[]
+    for grade in grades:
+        if grade < 38:
+            end.append(grade)
+        elif grade%5 <=2:
+            end.append(grade)
+        else:
+            end.append((grade//5 +1) *5)
+    return end
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
