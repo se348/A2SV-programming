8629acfde32627d7995aa9546c2acd3dfa9268a2
se348 <semir2578@gmail.com>
2022-07-07 21:05:49 +0300

    did sort list

---
+++ 148. Sort List/sort_list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        llist =[]
+        if head is None:
+            return None
+        tempo =head
+        while tempo:
+            llist.append(tempo.val)
+            tempo = tempo.next
+        llist.sort()
+        new_head= ListNode()
+        new_head.val = llist[0]
+        tempo = new_head
+        for n in range(1, len(llist)):
+            current = ListNode()
+            current.val = llist[n]
+            tempo.next = current
+            tempo = tempo.next
+        return new_head
