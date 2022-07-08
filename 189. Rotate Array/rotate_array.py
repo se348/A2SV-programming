e1fddeaa0152dc864ac032dca9255168819e0db3
se348 <semir2578@gmail.com>
2022-07-08 14:16:39 +0300

    did rotate array

---
+++ 189. Rotate Array/rotate_array.py
@@ -0,0 +1,9 @@
+from typing import List 
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for _ in range(k):
+            num = nums.pop()
+            nums.insert(0, num)
