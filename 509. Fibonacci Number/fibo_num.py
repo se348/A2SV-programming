643b4b45561bb1467d284e8813f6e2df9be4fd77
se348 <semir2578@gmail.com>
2022-09-07 13:50:29 +0300

    did fibonacci number

--- 509. Fibonacci Number/fibo_num.py
+++ 509. Fibonacci Number/fibo_num.py
@@ -1,7 +1,14 @@
 class Solution:
-        def fib(self, n: int) -> int:
-                if n ==0:
-                        return 0
-                if n==1:
-                        return 1
-                return self.fib(n-1) + self.fib(n-2)
+    def fib(self, n: int) -> int:
+        memo ={}
+        def helper(n):
+            if n==0:
+                return 0
+            if n==1:
+                return 1
+            if n in memo:
+                return memo[n]
+            ans = helper(n-1) + helper(n-2)
+            memo[n]= ans
+            return ans
+        return helper(n)
