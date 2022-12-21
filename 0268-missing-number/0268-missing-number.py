e0cb1aeb237951aedf7f971da273229e42a77a15
Semir Ahmed <66954610+se348@users.noreply.github.com>
2022-12-21 21:55:24 +0300

    Time: 340 ms (42.39%), Space: 15.1 MB (80.48%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        count =sum(nums)
+        count2 =sum(range(len(nums)+1))
+        return count2 -count
+        
+        
