f9bd1b9d795ad5bee634e312abf7d7827ea2a70d
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-01-05 19:22:20 +0300

    Time: 3366 ms (25.29%), Space: 60.1 MB (14.41%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        my_list= sorted(points)
+        arr=[]
+        common=None
+        for i in my_list:
+            if arr and arr[-1][-1]>=i[0] and common and (i[0]>= common[0] and i[0]<=common[1]):
+                common=list([max(common[0], i[0]),min(common[1], i[-1])])
+                arr[-1][-1] =i[1]
+            else:
+                common=[i[0], i[1]]
+                arr.append(i)
+        return len(arr)
+                                
+                                
