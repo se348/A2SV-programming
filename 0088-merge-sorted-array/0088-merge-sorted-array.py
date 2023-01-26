3ea7b8545d41e52577c6b226085dc06e8faa2d5f
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-01-26 06:19:00 +0300

    Time: 41 ms (65.00%), Space: 14 MB (32.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,23 +1,17 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-                
-            left=n-1
-            right=m-1
-            flag = m+n -1
-            while right >= 0 and  left >= 0:
-                if  nums1[right] > nums2[left]:
-                    nums1[flag] = nums1[right]
-                    right-=1
-                else:
+        
+        top = m - 1
+        bottom = n - 1
+        
+        for started_pos in range(m +n -1, -1, -1):
+            
+            if bottom != -1 and (top ==  -1 or nums2[bottom] >= nums1[top]):
+                nums1[started_pos] = nums2[bottom]
+                bottom -=1
+            
+            else:
+                nums1[started_pos] = nums1[top]
+                top -= 1
 
-                    nums1[flag] = nums2[left]
-                    left-=1
-                flag-=1
-            while right>=0:
-                nums1[flag] = nums1[right]
-                right-=1
-                flag-=1
-            while left>=0:
-                nums1[flag] = nums2[left]
-                left-=1
-                flag-=1
+        return nums1
