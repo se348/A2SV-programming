01ee57d1ac033d4f1d3d49df0060a5e87103f3d0
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-03-15 12:47:27 +0100

    Time: 47 ms (8.27%), Space: 13.9 MB (9.35%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,8 +1,26 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        if n % 2 ==0:
-            return int(pow(4, (n//2), (10** 9) + 7)  * pow(5,(n//2),(10**9)+7 ))% (10 **9 + 7)
-        return int(pow(4, (n//2), 10 ** 9 +7)  * pow(5 ,((n//2) + 1),10**9+7))% (10 **9 + 7)
+        memo ={0: 5, 1: 20}
+        temp = bin(n) 
         
-            
-    
+        arr_rep = []
+        
+        for i in range(2,len(temp)):
+            arr_rep.append(temp[i])
+        
+        
+        arr_rep.reverse()
+        
+        for i in range(2, len(arr_rep)):
+            memo[i] = pow(memo[i-1], 2, 10 ** 9 + 7)
+
+        res = 1
+        
+        for i in range(len(arr_rep)):
+            if int(arr_rep[i]):
+                res =(res * memo[i]) %( 10 **9 + 7)
+        return res
+        
+        
+        
+        
