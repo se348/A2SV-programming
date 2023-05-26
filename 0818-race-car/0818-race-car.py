323a0ae964ed8df35611b0c1fdf63a7bf0ba068b
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-05-26 21:14:32 +0200

    Time: 693 ms (32.77%), Space: 56.8 MB (27.61%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -13,7 +13,7 @@ class Solution:
             next1_pos, next1_speed = curr_pos + curr_speed, curr_speed * 2
             next2_pos, next2_speed = curr_pos, - 1
             
-            if next1_pos >= -1000 and next1_pos <= 3 * target and (next1_pos , next1_speed) not in visited:
+            if next1_pos >= -1000 and next1_pos <= 2 * target and (next1_pos , next1_speed) not in visited:
                 visited.add((next1_pos, next1_speed))
                 queue.append((next1_pos, next1_speed, curr_level + 1))
                 
