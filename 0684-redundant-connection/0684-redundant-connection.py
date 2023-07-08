669fdcfd7a5382c1c68c1ba3d1c0dbc44d4c0cef
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-07-08 19:56:19 +0200

    Time: 70 ms (68.95%), Space: 17 MB (35.55%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,20 +1,45 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        graph = collections.defaultdict(set)
-
-        def dfs(source, target):
-            if source not in visited:
-                visited.add(source)
-                if source == target: return True
+        n = len(edges)
+        
+        parent = [i for i in range(n)]
+        rank = [1 for i in range(n)]
+        
+        
+        def find(child):
+            if parent[child] == child:
+                return child
+            
+            parent[child] =  find(parent[child])
+            return parent[child]
+        
+        def union(c1, c2):
+            p1 , p2 = find(c1), find(c2)
+            
+            if p1 == p2:
+                return
+            
+            if rank[p1] > rank[p2]:
+                parent[p2] = parent[p1]
+                rank[p1] += rank[p2]
                 
-                for neighbor in graph[source]:
-                    curr= dfs(neighbor, target) 
-                    if curr:
-                        return curr
+            else:
+                parent[p1] = parent[p2]
+                rank[p2] += rank[p1]
 
-        for i, j in edges:
-            visited = set()
-            if i in graph and j in graph and dfs(i, j):
-                return i, j
-            graph[i].add(j)
-            graph[j].add(i)
+        
+        for a1, a2 in edges:
+            n1 = a1 - 1
+            n2 = a2 - 1
+            
+            prev1 = find(n1)
+            prev2 = find(n2)
+            
+            union(n1, n2)
+            
+            curr1 = find(n1)
+            curr2 = find(n2)
+            
+            if prev1 == curr1 and prev2 == curr2:
+                return [a1, a2]
+        
