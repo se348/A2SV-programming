7fbaeddab305a38d488dbaab07d38cb97d16e223
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-09-09 12:46:41 +0300

    Time: 41 ms (50.39%), Space: 16.2 MB (76.77%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -0,0 +1,27 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        
+        ver1_splitted = version1.split(".")
+        ver2_splitted = version2.split(".")
+        len_ver1 = len(ver1_splitted)
+        len_ver2 = len(ver2_splitted)
+        min_length = min(len_ver1, len_ver2)
+        
+        for i in range(min_length):
+            num1, num2 = int(ver1_splitted[i]), int(ver2_splitted[i])
+            if num1 < num2:
+                return -1
+            elif num1 > num2:
+                return 1
+        
+        for i in range(min_length, len_ver1):
+            if int(ver1_splitted[i]) != 0:
+                return 1
+        
+        for i in range(min_length, len_ver2):
+            if int(ver2_splitted[i]) != 0:
+                return -1
+            
+        return 0
+        
+        
