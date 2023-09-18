ea1757ae14970915c38cdaa3e8bb866d7828d076
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-09-18 14:36:18 +0300

    Update 0650-2-keys-keyboard.py

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,13 +1,17 @@
 class Solution:
+   
     def minSteps(self, n: int) -> int:
-        res = 0
+        if n == 1:
+            return 0
+        res= 0 
+        dp= [i for i in range(n + 1)]
         
-        prime = 2
+        for i in range(3, n+ 1):
+            for j in range(1, i + 1):
+                if (i % j == 0):
+                    multiples = (i// j)
+                    dp[i] = min(dp[i], dp[j] + multiples)
         
-        while prime <= n:
-            while n % prime ==0:
-                res += prime
-                n //= prime
-            prime += 1
+        return dp[n]
+                                
             
-        return res
