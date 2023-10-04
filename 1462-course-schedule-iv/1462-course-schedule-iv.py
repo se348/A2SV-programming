7d55116c0696dd6c20d19aa010423e10ad2d25eb
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-10-04 10:17:40 +0300

    Time: 1454 ms (18.62%), Space: 20.1 MB (76.48%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,28 +1,25 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        def constructGraph(prereq):
-            graph= collections.defaultdict(list)
-            for u,v in prereq:
-                graph[u].append(v)
-            return graph
-        graph = constructGraph(prerequisites)
-        source_dest={}
-        for source in dict(graph).keys():
-            queue= []
-            visited=set()
-            queue.append(source)
-            visited.add(source)
-            while queue:
-                curr_val = queue.pop(0)
-                for neigh in graph[curr_val]:
-                    if neigh not in visited:
-                        visited.add(neigh)
-                        queue.append(neigh)
-            source_dest[source] = list(visited)
-        ans=[]
-        for source, dest in queries:
-            if source in source_dest and  dest in source_dest[source]:
-                ans.append(True)
-            else:
-                ans.append(False)
-        return ans
+        
+        mat = [[False for _ in range(numCourses)] for _ in range(numCourses)]
+        
+        for src, dest in prerequisites:
+            mat[src][dest] = True
+        
+        
+        for k in range(numCourses):
+            
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    
+                    if mat[i][j]:
+                        continue
+                    
+                    mat[i][j] = mat[i][k] and mat[k][j]
+        
+        result = []
+        
+        for i, j in queries:
+            result.append(mat[i][j])
+        
+        return result
