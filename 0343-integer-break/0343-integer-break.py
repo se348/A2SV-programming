34e710d9f9dd7e77920b9e9c227df7e4f5885316
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-10-06 10:03:46 +0300

    Time: 37 ms (75.93%), Space: 16.3 MB (58.98%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,29 +1,28 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
         
-        @cache
-        def dp(curr_number):
-            if curr_number <= 2:
-                return 1
-            if curr_number == 3:
-                return 2
-            if curr_number == 4:
-                return 4
+        if n <= 2:
+            return 1
+        if n == 3:
+            return 2
+        
+        arr = [1] *( n + 1)
+        
+        for i in range(2, n + 1):
             
             res = 0
-            for num in range(1, curr_number):
-                diff = curr_number - num
-                
-                opt1 = diff * num
-                temp1, temp2 = dp(diff) , dp(num)
-                
-                opt2 = temp1 * temp2
-                opt3 = temp1 * num
-                opt4 = diff * temp2
-                
-                res =max(res, opt1, opt2, opt3, opt4)
             
-            return res
-       
-        return dp(n)
+            for j in range(1, i):
+                
+                diff = i - j
+                temp1= diff * j
+                temp2= arr[diff] * j
+                temp3= diff * arr[j]
+                temp4= arr[diff] * arr[j]
+                
+                res = max(temp1, temp2, temp3, temp4, res)
+            
+            arr[i] = res
+            
+        return arr[n]
     
