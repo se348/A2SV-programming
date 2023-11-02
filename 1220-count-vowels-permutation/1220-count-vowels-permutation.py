a9f1ae1a8b31e7da4e0c2764d2653cb4e7e7ea65
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-11-02 10:08:42 +0300

    Time: 553 ms (18.72%), Space: 16.2 MB (76.83%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -2,26 +2,19 @@ class Solution:
     def countVowelPermutation(self, n: int) -> int:
         rule = { "a" : ['e'], "e": ["a", "i"], "i":["a", "e", "o", "u"], "o": ["i", "u"], "u": ["a"]}
         
-        
-        @cache
-        def dp(remaining, curr_letter):
-            if remaining == 1:
-                return 1
-            
-            res = 0
-            
-            for neighbor in rule[curr_letter]:
-                res +=  dp(remaining - 1, neighbor)
-            
-            modulo = ((10 ** 9) + 7)
-            return res % modulo
-        
-        
-        res = 0
         chars = ["a", "e", "i", "o", "u"]
+        diction = {char: 1 for char in chars}
+        for i in range(n -1):
+            new_diction = defaultdict(int)
+            
+            for char in chars:
+                for neighbor in rule[char]:
+                    new_diction[char] = (new_diction[char] + diction[neighbor]) % (10 ** 9 + 7)
+            diction = new_diction
         
-        for char in chars:
-            res += dp(n, char)
+        total = 0
         
-        return (res % (10 ** 9 + 7))
+        for i in diction:
+            total += diction[i]
         
+        return (total % (10 ** 9 + 7))
