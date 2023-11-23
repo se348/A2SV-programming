4f59837e5278a5d748bfdffc56e7299639bd189c
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-11-23 10:57:20 +0300

    Time: 408 ms (5.03%), Space: 30.4 MB (34.84%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -2,20 +2,36 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         
         for i in range(len(nums)):
-            nums[i] = max(nums[i], 0)
+            if nums[i] <= 0:
+                nums[i] = max(nums[i], 0)
+            
+        left_ptr = 0
         
+        for right_ptr in range(len(nums)):
+            if nums[right_ptr] == 0:
+                continue
+            
+            while left_ptr < right_ptr and nums[left_ptr] != 0:
+                left_ptr += 1
+                
+            if nums[left_ptr] == 0:
+                nums[right_ptr], nums[left_ptr] = nums[left_ptr], nums[right_ptr]
+        
+        min_val = inf
+        for i in range(left_ptr + 1):
+            min_val = min(min_val, abs(nums[i]))
+        
+        if min_val != 1:
+            return 1
+        
+        for i in range(left_ptr + 1):
+            curr_num = abs(nums[i]) - 1
+            
+            if curr_num <= left_ptr:
+                nums[curr_num] = abs(nums[curr_num]) * -1
         
         for i in range(len(nums)):
-            val = abs(nums[i])
-            if 1 <= val <= len(nums):
-                if nums[val -1] >0:
-                    nums[val - 1] *= -1
-                elif nums[val - 1] == 0:
-                    nums[val - 1] = -1 *(len(nums) + 1)
-                    
-        for i in range(1, len(nums) + 1):
-            if nums[i - 1]>= 0:
-                return i
+            if nums[i] >= 0:
+                return (i + 1)
         return len(nums) + 1
-            
             
