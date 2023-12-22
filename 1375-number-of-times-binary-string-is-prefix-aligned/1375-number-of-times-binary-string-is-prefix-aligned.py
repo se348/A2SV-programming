5d7159f04973ded47e8d4b64d1ed49947dce3b56
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-12-22 17:50:30 +0300

    Time: 323 ms (98.90%), Space: 26.6 MB (5.49%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
@@ -7,13 +7,12 @@ class Solution:
         for i in range(len(flips)):
 
             visited.add(flips[i])
-            if inital in visited:
             
 
-                while inital in visited:
-                    inital += 1
-                
-                answer += inital == i+2
+            while inital in visited:
+                inital += 1
+
+            answer += inital == i+2
                 
         
         return answer
