a08a476abd48d0458c1c8bd768b06ec46abed74b
Semir Ahmed <66954610+se348@users.noreply.github.com>
2023-12-23 00:38:09 +0300

    Time: 35 ms (76.06%), Space: 17.2 MB (11.58%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.py
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -2,11 +2,8 @@ class Solution:
     def bulbSwitch(self, n: int) -> int:
         i = 1
         
-        count = 0
-        
         while (i ** 2) <= n:
-            count += 1
             i += 1
         
-        return count
+        return i - 1
         
