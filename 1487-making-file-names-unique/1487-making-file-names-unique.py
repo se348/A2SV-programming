b730b2225c9a360fc9a5758d21f30ff7973d23c2
Semir Ahmed <66954610+se348@users.noreply.github.com>
2024-01-13 11:05:27 +0300

    Time: 300 ms (88.61%), Space: 33 MB (5.94%) - LeetHub

--- 1487-making-file-names-unique/1487-making-file-names-unique.py
+++ 1487-making-file-names-unique/1487-making-file-names-unique.py
@@ -1,20 +1,22 @@
 class Solution:
     def getFolderNames(self, names: List[str]) -> List[str]:
         
+        
         occurances = {}
         res = []
         
         for key in names:
             if key not in occurances:
-                occurances[key] = 1
+                occurances[key] =  1
                 res.append(key)
+                continue
+                
+            occurances[key] += 1
             
-            else:
-                c = 1
-
-                while f"{key}({c})" in occurances:
-                    c += 1
-
-                occurances[f"{key}({c})"] = 1
-                res.append(f"{key}({c})")
+            while f"{key}({occurances[key] - 1})" in occurances:
+                occurances[key] += 1
+            occurances[f"{key}({occurances[key] - 1})"] = 1
+            res.append(f"{key}({occurances[key] - 1})")
+            
+            
         return res
