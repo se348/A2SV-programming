c41fd544454b4d1b20464271700767f812c60d4b
Semir Ahmed <66954610+se348@users.noreply.github.com>
2024-05-17 18:09:06 +0300

    Time: 401 ms (37.28%), Space: 73 MB (56.69%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,16 +1,32 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
-        @cache
-        def dp(s_ind, t_ind):
-            if t_ind == len(t):
-                return 1
-            if s_ind == len(s):
-                return 0
+#         @cache
+#         def dp(s_ind, t_ind):
+#             if t_ind == len(t):
+#                 return 1
+#             if s_ind == len(s):
+#                 return 0
             
-            if s[s_ind] == t[t_ind]:
-                return dp(s_ind + 1, t_ind + 1) + dp(s_ind + 1, t_ind)
+#             if s[s_ind] == t[t_ind]:
+#                 return dp(s_ind + 1, t_ind + 1) + dp(s_ind + 1, t_ind)
             
-            return dp(s_ind + 1, t_ind)
+#             return dp(s_ind + 1, t_ind)
         
-        return dp(0, 0)
+#         return dp(0, 0)
+        
+        # dp = [[0 for i in range(len(s)+1)] for j in range(len(t)+1)]
+        dp = [[0]*(len(t)+1) for i in range(len(s)+1)]
+        
+        for i in range(len(s)+1):
+            dp[i][len(t)] = 1
+        
+        for j in range(len(t)-1, -1, -1):
+            for i in range(len(s)-1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] += dp[i+1][j+1]
+                dp[i][j] += dp[i+1][j]
+        # print(dp)
+        return dp[0][0]
+                
+            
